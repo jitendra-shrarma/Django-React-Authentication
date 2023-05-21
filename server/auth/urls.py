c4eaec1f80171ce7482d11ec6581937fa973f51e
Jitendra Sharma <jitendra.sharma2020@outlook.com>
2023-05-21 18:23:00 +0530

    update settings for mail and urls for auth api

---
+++ server/auth/urls.py
@@ -0,0 +1,17 @@
+# Import path function and django_rest_passwordreset module
+import django_rest_passwordreset
+from django.urls import path, include
+
+# Import SignUpAPIView
+from .views import SignUpAPIView, SignInAPIView, SignOutAPIView, UserViewAPIView
+
+
+# Create urlpatterns
+urlpatterns = [
+    # path for auth/signup, response for signup with SignUpAPIView
+    path("auth/signup", SignUpAPIView.as_view(), name="signup"),
+    # path for auth/signin, response for signin with SignInAPIView
+    path("auth/signin", SignInAPIView.as_view(), name="signin"),
+    # path for auth/signout, response for signout with SignOutAPIView
+    path("auth/signout", SignOutAPIView.as_view(), name="signout"),
+]
